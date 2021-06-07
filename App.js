14323e0ae2ca7293fe62dd514a6e8f3775785586
za-n-6 <zara.nasim96@gmail.com>
2021-06-07 13:40:32 +0500

    my first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{textAlign: 'center'}}>Try editing me!!!🎉</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
